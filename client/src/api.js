f45afe46ac4ad0cc6482d5fc3d0dcddaf11ef970
taewoong heo <ihtwandy@naver.com>
2025-10-03 21:53:06 +0900

    feat: Add console.log to debug VITE_API_BASE_URL

--- client/src/api.js
+++ client/src/api.js
@@ -1,3 +1,4 @@
+console.log("VITE_API_BASE_URL from Vercel:", import.meta.env.VITE_API_BASE_URL);
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:3001';
 
 export default function api(path){
